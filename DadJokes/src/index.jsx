627f01946dd68c9fb9ccf739118f1de6ab5b3ae8
Milota Styková <you@example.com>
2022-05-28 13:59:20 +0200

    ukol c. 14

--- DadJokes/src/index.jsx
+++ DadJokes/src/index.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
-const App = () => (
-  <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
-  </div>
-);
+const App = () => {
+  const [upLikes, setUpLikes] = useState(0);
+  const [downLikes, setDownLikes] = useState(0);
 
-createRoot(
-  document.querySelector('#app')
-).render(<App />);
+  return (
+    <div class="container">
+      <div class="joke">
+        <div class="joke__body">
+          <div class="joke__user">
+            <img
+              class="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p class="user-name">Neroxx</p>
+          </div>
+
+          <p class="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div class="joke__likes">
+          <button
+            id="btn-up"
+            class="btn-like btn-like--up"
+            onClick={() => {
+              setUpLikes(upLikes + 1);
+            }}
+          ></button>
+          <span id="likes-up" class="likes-count likes-count--up">
+            {upLikes}
+          </span>
+          <button
+            id="btn-down"
+            class="btn-like btn-like--down"
+            onClick={() => {
+              setDownLikes(downLikes + 1);
+            }}
+          ></button>
+          <span id="likes-down" class="likes-count likes-count--down">
+            {downLikes}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+createRoot(document.querySelector('#app')).render(<App />);
