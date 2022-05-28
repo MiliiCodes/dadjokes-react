4516b39115bf88958b9ef021e9851bf633801b8b
Milota Styková <you@example.com>
2022-05-28 14:05:56 +0200

    zmena class na className v jsx

--- DadJokes/src/index.jsx
+++ DadJokes/src/index.jsx
@@ -7,42 +7,42 @@ const App = () => {
   const [downLikes, setDownLikes] = useState(0);
 
   return (
-    <div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
             <img
-              class="user-avatar"
+              className="user-avatar"
               src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
             />
-            <p class="user-name">Neroxx</p>
+            <p className="user-name">Neroxx</p>
           </div>
 
-          <p class="joke__text">
+          <p className="joke__text">
             The secret service isn't allowed to yell "Get down!" anymore when
             the president is about to be attacked. Now they have to yell
             "Donald, duck!"
           </p>
         </div>
-        <div class="joke__likes">
+        <div className="joke__likes">
           <button
             id="btn-up"
-            class="btn-like btn-like--up"
+            className="btn-like btn-like--up"
             onClick={() => {
               setUpLikes(upLikes + 1);
             }}
           ></button>
-          <span id="likes-up" class="likes-count likes-count--up">
+          <span id="likes-up" className="likes-count likes-count--up">
             {upLikes}
           </span>
           <button
             id="btn-down"
-            class="btn-like btn-like--down"
+            className="btn-like btn-like--down"
             onClick={() => {
               setDownLikes(downLikes + 1);
             }}
           ></button>
-          <span id="likes-down" class="likes-count likes-count--down">
+          <span id="likes-down" className="likes-count likes-count--down">
             {downLikes}
           </span>
         </div>
